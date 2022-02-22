1ca94a6901d643435989d2582f0946bb23cd20b3
irene <keiko103@bu.edu>
2022-02-23 07:15:33 +0800

    add lottery code

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,50 @@
 <template>
-  <div class="board">
-    <div v-for="(item, index) in matrix" :key="index">
+  <div class="container">
+    <div class="title">神奇轉盤 #069</div>
+    <div class="board">
+      <!-- <div class="loading" v-show="isLoading">
+        <default-loading></default-loading>
+      </div> -->
+      <transition>
+        <div class="xCoord" v-show="isShowCoord">
+          <div v-for="(item, index) in matrix" :key="index">{{ index + 1 }}</div>
+        </div>
+      </transition>
+      <transition>
+        <div class="yCoord" v-show="isShowCoord">
+          <div v-for="(item, index) in matrix" :key="index">{{ index + 1 }}</div>
+        </div>
+      </transition>
       <div
-        v-for="(child, childIndex) in item" :key="childIndex"
-        class="block"
-        :style="{ 'background-color': `${child.color}` }"
-      ></div>
+        v-for="(item, index) in matrix"
+        :key="index"
+      >
+      <!-- :style="{ opacity: isLoading ? '.3': '1' }" -->
+        <div
+          v-for="(child, childIndex) in item" :key="`${index}_${childIndex}`"
+          class="block"
+          :style="{ 'background-color': `${child.color}` }"
+        ></div>
+      </div>
     </div>
+    <div class="toolbar">
+      <v-btn color="primary" small @click="handleGenCoord" :disabled="isLoading">generate</v-btn>
+      <v-btn class="ml-2" color="warning" small @click="handleRevertCoord" :disabled="blackList.length === 0">revert</v-btn>
+      <v-btn class="ml-2" small @click="handleClearCoord">clear</v-btn>
+      <v-btn
+        class="ml-2"
+        color="secondary"
+        small
+        @click="handleShowCoord"
+      >{{ isShowCoord ? 'hide': 'show' }}</v-btn>
+    </div>
+    <div class="display">latest result: {{ lastGenDisplay }}</div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue, Watch } from 'nuxt-property-decorator'
+import { Component, Vue } from 'nuxt-property-decorator'
+import DefaultLoading from '../components/DefaultLoading.vue'
 import { $axios } from '~/utils/api'
 
 interface Block {
@@ -24,16 +57,30 @@ interface Coord {
 }
 
 @Component({
-  layout: 'default'
+  layout: 'default',
+  components: {
+    DefaultLoading
+  }
 })
 export default class Index extends Vue {
-  // please move matrix generation to the backend, frontend is just for display
-
   private matrixLength = 20
 
   private matrix: Block[][] = []
 
   private blackList: Coord[] = []
+
+  private isShowCoord = true
+
+  private isLoading = false
+
+  private lastGen: number[] = []
+
+  private get lastGenDisplay() {
+    if (this.lastGen.length === 2) {
+      return [this.lastGen[0] + 1, this.lastGen[1] + 1]
+    }
+    return []
+  }
 
   private getSquareMatrix = (size: number) => {
     return Array(size)
@@ -72,37 +119,29 @@ export default class Index extends Vue {
     const _blockColor = this.getRandomColor()
     _target.color = _blockColor
     this.blackList.push({ row: _row, col: _col })
+    this.lastGen = [_row, _col]
     return true
   }
 
   private setMatrixInit() {
     this.matrix = [...this.getMatrixInterface(this.matrixLength)]
+    this.lastGen = []
+    this.blackList = []
   }
 
   private intervalId: any = null
 
-  private setRandomBlockGen() {
-    this.intervalId = setInterval(() => {
-      if (!this.setRandomBlock()) {
-        this.setRandomBlock()
-      } else if (this.blackList.length === this.matrix.length * this.matrix[0].length) {
-        // matrix full
-        clearInterval(this.intervalId)
-        // TODO send email
-        this.genEmail()
-        this.setMatrixInit()
-      }
-    }, 1000)
-  }
-
-  private async genEmail() {
-    try {
-      // email stored on backend
-      await $axios.post('/api/v1/endpoint', { timestamp: '', canvas: this.matrix })
-    } catch (e: unknown) {
-
-    }
-  }
+  // private setRandomBlockGen() {
+  //   this.intervalId = setInterval(() => {
+  //     if (!this.setRandomBlock()) {
+  //       this.setRandomBlock()
+  //     } else if (this.blackList.length === this.matrix.length * this.matrix[0].length) {
+  //       // matrix full
+  //       clearInterval(this.intervalId)
+  //       this.setMatrixInit()
+  //     }
+  //   }, 1000)
+  // }
 
   private getMatrixInterface(val: number) {
     const _blocks = this.getSquareMatrix(val)
@@ -116,9 +155,45 @@ export default class Index extends Vue {
     return _result
   }
 
+  private handleShowCoord() {
+    this.isShowCoord = !this.isShowCoord
+  }
+
+  private handleClearCoord() {
+    this.setMatrixInit()
+  }
+
+  private handleRevertCoord() {
+    if (this.blackList.length) {
+      const _row = this.blackList[this.blackList.length - 1].row
+      const _col = this.blackList[this.blackList.length - 1].col
+      this.matrix[_row][_col].color = ''
+      this.blackList.pop()
+      if (this.blackList.length) {
+        const _rowNew = this.blackList[this.blackList.length - 1].row
+        const _colNew = this.blackList[this.blackList.length - 1].col
+        this.lastGen = [_rowNew, _colNew]
+      } else {
+        this.lastGen = []
+      }
+    }
+  }
+
+  private handleGenCoord() {
+    this.isLoading = true
+    const _timeoutId = setTimeout(() => {
+      this.isLoading = false
+      if (!this.setRandomBlock()) {
+        this.handleGenCoord()
+        clearTimeout(_timeoutId)
+        return
+      }
+      clearTimeout(_timeoutId)
+    }, 300)
+  }
+
   private created() {
     this.setMatrixInit()
-    this.setRandomBlockGen()
   }
 
   private beforeDestroy() {
@@ -129,17 +204,78 @@ export default class Index extends Vue {
   }
 }
 </script>
+
 <style lang="scss" scoped>
+.container {
+  margin-left: auto;
+  margin-right: auto;
+  width: 360px;
+}
+
+.title {
+  text-align: center;
+}
+
 .board {
+  position: relative;
   display: flex;
   align-items: center;
   justify-content: center;
+  min-height: 380px;
+  padding-top: 20px;
+}
+
+.loading {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.toolbar {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 12px;
+}
+
+.display {
+  margin-top: 16px;
+}
+
+.xCoord {
+  position: absolute;
+  top: 0;
+  display: flex;
   min-width: 360px;
-  min-height: 180px;
+  > div {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    width: 18px;
+    font-size: 12px;
+    color: rgba(0, 0, 0, .66);
+  }
+}
+
+.yCoord {
+  position: absolute;
+  top: 20px;
+  left: -36px;
+  min-width: 360px;
+  > div {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    width: 18px;
+    font-size: 12px;
+    color: rgba(0, 0, 0, .66);
+  }
 }
 
 .block {
   width: 18px;
   height: 18px;
+  border: 1px solid rgba(0, 0, 0, .1);
+  transition: background-color .3s;
 }
 </style>
